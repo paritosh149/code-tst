9c08675f9b3ed5ce84ebc80bad39d7ca4b6a12d7
Paritosh Mishra <paritosh149@outlook.com>
2021-11-17 03:52:02 +0000

    test cases added for 02

--- src/02_get-first-non-repeating-char.spec.js
+++ src/02_get-first-non-repeating-char.spec.js
@@ -1,4 +1,5 @@
 const solution = require('./02_get-first-non-repeating-char')
+const { InvalidInputError } = require('./constants')
 
 test('Test 1 - "this is the string"', () => {
   expect(solution('this is the string')).toBe('e')
@@ -10,4 +11,12 @@ test('Test 2 - "persuit is a good place to work"', () => {
 
 test('Test 3 - "we have free coffee"', () => {
   expect(solution('we have free coffee')).toBe('w')
+})
+
+test('Test 4 - Non-string object should throw Error', () => {
+  expect(() => solution({})).toThrow(InvalidInputError)
+})
+
+test('Test 5 - String with no unique character should return null', () => {
+  expect(solution('aa ee')).toBe(null)
 })
